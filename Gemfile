3ba4b26ad49e65b455af087abc3b9f0adba37e9c
Joe Van Dyk <joe@fixieconsulting.com>
2011-06-15 10:52:26 -0700

    Using sequel_pg

--- Gemfile
+++ Gemfile
@@ -2,3 +2,4 @@ source "http://rubygems.org"
 
 gem "pg"
 gem "sequel"
+gem "sequel_pg"
--- test.rb
+++ test.rb
@@ -1,16 +1,36 @@
 require 'rubygems'
 require 'bundler/setup'
 require 'sequel'
-require 'sequel/extensions/migration'
+require 'sequel/adapters/postgres'
+require 'sequel_pg'
+require 'benchmark'
 
 DB = Sequel.connect('postgres://localhost/test')
 
 items = DB[:items] # Create a dataset
 
-# Populate the table
-items.insert(:name => 'abc', :price => rand * 100, :sku => 'first')
-items.insert(:name => 'def', :price => rand * 100, :sku => 'second')
-items.insert(:name => 'ghi', :price => rand * 100, :sku => 'third')
+Benchmark.bm do |x|
+  x.report "transaction" do
+    5000.times do
+      DB.transaction do
+        # Populate the table
+        items.insert(:name => 'abc', :price => rand * 100, :sku => 'first')
+        items.insert(:name => 'def', :price => rand * 100, :sku => 'second')
+        items.insert(:name => 'ghi', :price => rand * 100, :sku => 'third')
+      end
+    end
+  end
+
+  x.report "no transaction" do
+    5000.times do
+      # Populate the table
+      items.insert(:name => 'abc', :price => rand * 100, :sku => 'first')
+      items.insert(:name => 'def', :price => rand * 100, :sku => 'second')
+      items.insert(:name => 'ghi', :price => rand * 100, :sku => 'third')
+    end
+  end
+
+end
 
 # Print out the number of records
 puts "Item count: #{items.count}"
