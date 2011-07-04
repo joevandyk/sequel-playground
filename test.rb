21e57a59a52a61c2c616e646737b06c9833940ff
Joe Van Dyk <joe@fixieconsulting.com>
2011-07-03 23:10:50 -0700

    updates

--- test.rb
+++ test.rb
@@ -3,35 +3,11 @@ require 'bundler/setup'
 require 'sequel'
 require 'benchmark'
 
-DB = Sequel.connect('postgres://localhost/test')
+DB = Sequel.connect(ENV['DATABASE_URL'])
 
-items = DB[:items] # Create a dataset
+items = DB[:orders] # Create a dataset
+puts items.count
 
-Benchmark.bm do |x|
-  x.report "transaction" do
-    5000.times do
-      DB.transaction do
-        # Populate the table
-        items.insert(:name => 'abc', :price => rand * 100, :sku => 'first')
-        items.insert(:name => 'def', :price => rand * 100, :sku => 'second')
-        items.insert(:name => 'ghi', :price => rand * 100, :sku => 'third')
-      end
-    end
-  end
-
-  x.report "no transaction" do
-    5000.times do
-      # Populate the table
-      items.insert(:name => 'abc', :price => rand * 100, :sku => 'first')
-      items.insert(:name => 'def', :price => rand * 100, :sku => 'second')
-      items.insert(:name => 'ghi', :price => rand * 100, :sku => 'third')
-    end
-  end
-
-end
-
-# Print out the number of records
-puts "Item count: #{items.count}"
-
-# Print out the average price
-puts "The average price is: #{items.avg(:price)}"
+DB.run "set search_path to myschema;"
+items = DB[:distributors;] # Create a dataset
+puts items.count
